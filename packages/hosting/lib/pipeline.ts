b968f3f5f6497fdc03064a360741a32b7ddeba49
Dany Alawwa <dany.alawwa+git@gmail.com>
2023-09-17 17:52:21 +0200

    remove role

--- packages/hosting/lib/pipeline.ts
+++ packages/hosting/lib/pipeline.ts
@@ -6,39 +6,8 @@ import { CompositePrincipal, Effect, PolicyStatement, Role, ServicePrincipal } f
 export class Pipeline extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const role = new Role(this, "pipeline", {
-      roleName: "pipeline",
-      assumedBy: new CompositePrincipal(
-        new ServicePrincipal("codebuild.amazonaws.com"),
-        new ServicePrincipal("codepipeline.amazonaws.com"),
-        )
-    })
-    const policyStatement = new PolicyStatement({
-        actions: [
-          "s3:GetObject*",
-          "s3:GetBucket*",
-          "s3:List*",
-          "s3:DeleteObject*",
-          "s3:PutObject",
-          "s3:Abort*",
-      ],
-      effect: Effect.ALLOW,
-      resources: ["*"],
-    });
-
-    role.addToPolicy(
-        policyStatement,
-    );
-    role.addToPolicy(
-      new PolicyStatement({
-        actions: ['sts:AssumeRole'],
-        effect: Effect.ALLOW,
-        resources: [process.env.PIPELINE_ROLE_ARN!],
-      })
-    )
+    
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      role: role.withoutPolicyUpdates(),
       synth: new pipelines.ShellStep('Synth', {
         input: pipelines.CodePipelineSource.connection('dalawwa/dawalnut', 'main', {
           connectionArn: process.env.CONNECTION_ARN!,
