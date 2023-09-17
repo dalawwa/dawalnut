840b88229526bba6a8f847bbb7e4adb6a4acd745
Dany Alawwa <dany.alawwa+git@gmail.com>
2023-09-17 16:02:08 +0200

    create custom role for the pipeline

--- packages/hosting/lib/pipeline.ts
+++ packages/hosting/lib/pipeline.ts
@@ -2,15 +2,26 @@ import { Stack, StackProps, pipelines } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 import { DevStage } from "./stage-dev";
+import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 export class Pipeline extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const role = new Role(this, 'Role', {
+      assumedBy: new ServicePrincipal('codepipeline.amazonaws.com'),
+      description: 'This is a custom role for codepipeline to assume',
+    });
+
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      role,
       synth: new pipelines.ShellStep('Synth', {
         input: pipelines.CodePipelineSource.connection('dalawwa/dawalnut', 'main', {
           connectionArn: process.env.CONNECTION_ARN!,
         }),
+        env: {
+          AWS_ACCOUNT: process.env.AWS_ACCOUNT!,
+          AWS_REGION: process.env.AWS_REGION!,
+        },
         installCommands: ['npm i -g npm@9.6.7'],
         commands: [
           'node -v',
@@ -20,6 +31,7 @@ export class Pipeline extends Stack {
         ],
         primaryOutputDirectory: 'packages/hosting/cdk.out',
       }),
+      dockerEnabledForSynth: true
     });
 
     pipeline.addStage(new DevStage(this, 'Dev'));
