f033bf5af955d20b99b7ffcb67375821ed87d6e6
Dany Alawwa <dany.alawwa+git@gmail.com>
2023-09-17 21:37:13 +0200

    more stuff

--- packages/hosting/lib/pipeline.ts
+++ packages/hosting/lib/pipeline.ts
@@ -1,63 +1,16 @@
-import { Stack, StackProps, pipelines } from "aws-cdk-lib";
+import { Stack, StackProps, aws_codepipeline} from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 import { DevStage } from "./stage-dev";
-import { PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 export class Pipeline extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const role = new Role(this, 'Role', {
-      assumedBy: new ServicePrincipal('codepipeline.amazonaws.com'),
-    });
-
-    role.addToPolicy(new PolicyStatement({
-      actions: [
-        "sts:AssumeRole",
-        "secretsmanager:GetSecretValue",
-        "secretsmanager:DescribeSecret",
-        "cloudformation:*",
-        "s3:*",
-      ],
-      resources: ['*'],
-    }));
-
-    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      role,
-      synth: new pipelines.ShellStep('Synth', {
-        input: pipelines.CodePipelineSource.connection('dalawwa/dawalnut', 'main', {
-          connectionArn: process.env.CONNECTION_ARN!,
-        }),
-        installCommands: ['npm i -g npm@9.6.7'],
-        commands: [
-          'node -v',
-          'npm ci',
-          'npm run build -w hosting',
-          'npm run synth -w hosting',
-        ],
-        primaryOutputDirectory: 'packages/hosting/cdk.out',
-      }),
-      dockerEnabledForSynth: true,
-      dockerEnabledForSelfMutation: true,
-      cliVersion: '2.96.2',
-      selfMutation: true,
-      codeBuildDefaults: {
-        rolePolicy: [
-          new PolicyStatement({
-            actions: [
-              "sts:AssumeRole",
-              "secretsmanager:GetSecretValue",
-              "secretsmanager:DescribeSecret",
-              "cloudformation:*",
-              "s3:*",
-            ],
-            resources: ['*'],
-          }),
-        ]
-      },
-    });
+    const pipeline = new codepipeline.Pipeline(this, 'CodePipeline');
 
     pipeline.addStage(new DevStage(this, 'Dev', {
+      stageName: 'Dev',
       env: {
         account: '765757105156',
         region: 'eu-central-1',
