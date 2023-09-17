ce5db803b226c24623a6bb350f62b16177328ad0
Dany Alawwa <dany.alawwa+git@gmail.com>
2023-09-17 17:39:49 +0200

    setup role

--- packages/hosting/lib/pipeline.ts
+++ packages/hosting/lib/pipeline.ts
@@ -2,16 +2,38 @@ import { Stack, StackProps, pipelines } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 import { DevStage } from "./stage-dev";
-import { PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { CompositePrincipal, Effect, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 export class Pipeline extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const role = new Role(this, "pipeline", {
+      roleName: "pipeline",
+      assumedBy: new CompositePrincipal(
+        new ServicePrincipal("codebuild.amazonaws.com"),
+        new ServicePrincipal("codepipeline.amazonaws.com"),
+        )
+    })
+    const policyStatement = new PolicyStatement({
+        actions: [
+          "s3:GetObject*",
+          "s3:GetBucket*",
+          "s3:List*",
+          "s3:DeleteObject*",
+          "s3:PutObject",
+          "s3:Abort*",
+      ],
+      effect: Effect.ALLOW,
+      resources: [
+          "*"
+      ],
+    });
+
+    role.addToPolicy(
+        policyStatement,
+    );
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      role: new Role(this, "pipeline", {
-        roleName: "pipeline",
-        assumedBy: new ServicePrincipal("codepipeline.amazonaws.com")
-      }).withoutPolicyUpdates(),
+      role: role.withoutPolicyUpdates(),
       synth: new pipelines.ShellStep('Synth', {
         input: pipelines.CodePipelineSource.connection('dalawwa/dawalnut', 'main', {
           connectionArn: process.env.CONNECTION_ARN!,
